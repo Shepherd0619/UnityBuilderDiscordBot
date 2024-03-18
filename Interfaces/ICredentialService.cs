4646553d58417fd2401780aa0dbf7271ac53ba59
Shepherd0619 <shepherd0619@outlook.com>
2024-03-18 17:33:48 +0800

    [add] ICredentialService

---
+++ Interfaces/ICredentialService.cs
@@ -0,0 +1,12 @@
+﻿namespace UnityBuilderDiscordBot.Interfaces;
+
+public interface ICredentialService
+{
+    public void Login();
+
+    public void Logout();
+
+    public void SetCredential();
+
+    public void GetCredential();
+}
