a9a28988b39a34339b3432d1d34d29b7dac7fe39
Shepherd0619 <shepherd0619@outlook.com>
2024-04-03 11:40:47 +0800

    [add] ISourceControlService

---
+++ Interfaces/ISourceControlService.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using UnityBuilderDiscordBot.Models;
+
+namespace UnityBuilderDiscordBot.Interfaces;
+
+public interface ISourceControlService
+{
+    public string WorkingDir { get; set; }
+    
+    public string CurrentBranch { get; set; }
+    
+    public string CurrentCommit { get; set; }
+    
+    public Process RunningProcess { get; set; }
+    
+    public Task<ResultMsg> Checkout(string branch);
+
+    public Task<ResultMsg> Reset(bool hard);
+}
